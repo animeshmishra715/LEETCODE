ab15be96f72efae0c61eeac8de71978f55a0e8e1
DEPLONE <animeshmishra1321@gmail.com>
2026-01-27 03:56:38 +0530

    Time: 44 ms (10.52%) | Memory: 47.8 MB (19.02%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -1,26 +1,24 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int []hash=new int[100000+1];
-        for(int i=0;i<nums.length;i++){
-            hash[nums[i]]+=1;
+        int[] hash = new int[100000 + 1];
+        for (int i = 0; i < nums.length; i++) {
+            hash[nums[i]] += 1;
         }
-        int d=Integer.MAX_VALUE;
-        int c=Integer.MIN_VALUE;
-        for(int i=0;i<hash.length;i++){
-            if(i%2==0){
-            c=Math.max(c,hash[i]);
+        int c = 0;
+        int d = -1;
+
+        for (int i = 0; i < hash.length; i++) {
+            if (i % 2 == 0 && hash[i] > 0) {
+                if (hash[i] > c) {
+                    c = hash[i];
+                    d = i;
+                } else if (hash[i] == c) {
+                    d = Math.min(d, i);
+                }
             }
-            
         }
-        if(c==0) return -1;
-        for(int i=0;i<hash.length;i++){
-            if(i%2==0){
-            if(hash[i]==c){
-                d=Math.min(d,i);
-            }
-            }
-            
-        }
+
+
         return d;
     }
 }
