b7aac36dd83e4df915c032e10c0cbcd28e419f92
DEPLONE <animeshmishra1321@gmail.com>
2026-02-19 02:44:05 +0530

    Time: 1 ms (60.12%) | Memory: 47.6 MB (30.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,42 +1,35 @@
 class Solution {
-    public static int[][] markrow(int [][]matrix,int i){
-        for(int j=0;j<matrix[i].length;j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-10;
-            }
-            
-        }
-        return matrix;
-    }
-    public static int[][] markcol(int [][]matrix,int j){
-        for(int i=0;i<matrix.length;i++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-10;
-            }
-            
-        }
-        return matrix;
-    }
     public void setZeroes(int[][] matrix) {
-        
-        for(int i=0;i<matrix.length;i++){
-                for(int j=0;j<matrix[i].length;j++){
-                    if(matrix[i][j]==0){
-                        markrow(matrix,i);
-                        markcol(matrix,j);
-                    }
-                }
-            }
-        
+        int col0=1;
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==-10){
-                    matrix[i][j]=0;
+                if(matrix[i][j]==0){
+                    if(j==0){
+                        col0=0;
+                    }else{
+                    matrix[0][j]=0;
+                    }
+                    matrix[i][0]=0;
                 }
             }
         }
-        
-}
-    
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[i].length;j++){
+                if(matrix[i][j]!=0){
+                if(matrix[0][j]==0||matrix[i][0]==0){
+                    matrix[i][j]=0;
 
+                }
+                
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=0;j<matrix[0].length;j++) matrix[0][j]=0;
+        }
+        if(col0==0){
+            for(int i=0;i<matrix.length;i++) matrix[i][0]=0;
+        }
+
+    }
 }
