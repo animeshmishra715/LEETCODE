5cd92634459fdbf71babfea3968d67e1920f4c92
DEPLONE <animeshmishra1321@gmail.com>
2026-02-19 03:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (14.61%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int temp1=0;
+        int temp2=0;
+        int arr[][]=new int[matrix.length][matrix.length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length;j++){
+                arr[j][(matrix.length-1)-i]=matrix[i][j];
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length;j++){
+                matrix[i][j]=arr[i][j];
+            }
+        }
+    }
+}
