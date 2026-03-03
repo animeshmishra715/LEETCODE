44a87e37817e9b886ca0276be3b4c7d864508c99
DEPLONE <animeshmishra1321@gmail.com>
2026-03-04 03:20:44 +0530

    Time: 94 ms (5.59%) | Memory: 58.9 MB (5.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -5,10 +5,7 @@ class Solution {
         for (int x : nums) {
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
-        int max = 0;
-        for (int value : map.values()) {
-            max = Math.max(value, max);
-        }
+        
         List<Integer> ans = new ArrayList<>();
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             if(entry.getValue()>(nums.length)/3){
