190bb68449a56af12f0dd33560321cd8b8e1d1db
DEPLONE <animeshmishra1321@gmail.com>
2026-04-09 09:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (27.57%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+       // ListNode temp=head;
+       // int count =0;
+        //bruteforce
+    //     while(temp!=null){
+    //         count++;
+    //         temp=temp.next;
+    //     }
+    //    if(count==n) {
+    //     ListNode newhead=head.next;
+    //     return newhead;
+    //    }
+    //    int end=count-n;
+    //    ListNode a=head;
+    //    while(a!=null){
+    //     end--;
+    //     if(end==0){
+    //         break;
+    //     }
+    //     a=a.next;
+        
+    //    }
+    //    ListNode delteNode=a.next;
+    //     a.next=a.next.next;
+    //     return head;
+        ListNode fast=head;
+        for(int i=0;i<n;i++) fast=fast.next;
+        if(fast==null) return head.next;
+        ListNode slow=head;
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        
+        ListNode deleteNode=slow.next;
+        slow.next=slow.next.next;
+        return head;
+
+    }
+}
